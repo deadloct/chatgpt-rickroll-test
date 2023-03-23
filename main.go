7487264108903a6be52781470d9d0b761e0d929a
Deadloct <rad@deadloct.com>
2023-03-23 05:40:50 -0700

    Changes needed to make it work

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 )
+
+const RickRollCMDName = "rickroll"
 
 func main() {
 	// Set up the Discord session
@@ -26,13 +29,23 @@ func main() {
 		return
 	}
 
+	cmd := &discordgo.ApplicationCommand{
+		Name:        RickRollCMDName,
+		Description: "show the rickroll youtube link",
+	}
+	if _, err := discord.ApplicationCommandCreate(discord.State.User.ID, "", cmd); err != nil {
+		panic(err)
+	}
+
 	// Keep the program running
 	fmt.Println("Bot is now running. Press CTRL-C to exit.")
-	http.ListenAndServe(":8080", nil)
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
 }
 
 func handleSlashCommand(session *discordgo.Session, interaction *discordgo.InteractionCreate) {
-	if interaction.Data.Name == "rickroll" {
+	if interaction.ApplicationCommandData().Name == RickRollCMDName {
 		// Send the user the official music video for "Never Gonna Give You Up"
 		response := discordgo.InteractionResponse{
 			Type: discordgo.InteractionResponseChannelMessageWithSource,
